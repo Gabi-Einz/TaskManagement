5a9d8d4c5924952a496dbdd78e2047fa5b55572d
Gabriel Vargas <gvargas@gestionatupyme.cl>
2025-04-08 00:32:53 -0300

    prisma orm configured

---
+++ src/shared/interfaces/IOrmRepository.ts
@@ -0,0 +1,8 @@
+export interface IOrmRepository<T> {
+  create(data: Partial<T>): Promise<T>;
+  updateById(id: number, data: Partial<T>): Promise<T>;
+  deleteById(id: number): Promise<T>;
+  findById(id: number): Promise<T | null>;
+  findAll(): Promise<T[]>;
+  findByName(name: string): Promise<T | null>;
+}
